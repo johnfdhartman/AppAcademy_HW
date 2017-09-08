61e18b3a5742b35fd3e83e7315211c2d8a8ef0b7
John Hartman <john.f.d.hartman@gmail.com>
2017-09-08 00:11:56 -0700

    aaaa

---
+++ W6D5/npm-getting-started/webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: './entry.jsx',
+  output: './output.jsx',
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  }
+};
