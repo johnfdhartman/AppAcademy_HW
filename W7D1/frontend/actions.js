25e8aeed2ca55cba0c957dd3af52f74d260b7c20
John Hartman <john.f.d.hartman@gmail.com>
2017-09-11 01:33:32 -0700

    currency

---
+++ W7D1/frontend/actions.js
@@ -0,0 +1,9 @@
+function selectCurrency(baseCurrency, rates) {
+  return {
+    type: 'SWITCH_CURRENCY',
+    baseCurrency: baseCurrency,
+    rates: rates
+  };
+}
+
+export default selectCurrency;
---
+++ W7D1/frontend/reducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  baseCurrency: "Please select",
+  rates: {}
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+
+};
+
+export default reducer;
