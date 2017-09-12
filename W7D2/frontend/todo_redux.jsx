41693d7d45d700b42508b0fe51aa81114f15f4d3
John Hartman <john.f.d.hartman@gmail.com>
2017-09-12 00:39:18 -0700

    W7D2

---
+++ W7D2/frontend/todo_redux.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  const store = configureStore(preloadedState);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
+
+const addLoggingToDispatch = (store) => {
+  const storeDispatch = store.dispatch;
+  return function(next) {
+    return function(action) {
+      console.log(store.getState());
+      console.log(action);
+      next(action);
+      console.log(store.getState());
+    };
+  };
+};
